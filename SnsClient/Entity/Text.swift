731d8042aaa1ae782a76d476a5b54c2f3c9be4d8
div-antk <51283468+owaii@users.noreply.github.com>
2021-07-19 23:15:21 +0900

    TextのEntity作成

---
+++ SnsClient/Entity/Text.swift
@@ -0,0 +1,13 @@
+//
+//  Text.swift
+//  SnsClient
+//
+//  Created by Takuya Ando on 2021/07/19.
+//
+
+import Foundation
+
+public struct TextResponse: Codable {
+    let text: String
+    let _created_at: String
+}
