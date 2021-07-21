5ed60f4066536992b26e7327b75724d7b7263846
div-antk <51283468+owaii@users.noreply.github.com>
2021-07-21 23:49:46 +0900

    VMの実装

---
+++ SnsClient/ViewModel/PostViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  PostViewModel.swift
+//  SnsClient
+//
+//  Created by Takuya Ando on 2021/07/21.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+import Moya
+
+protocol PostViewModelOutputs {
+    var posts: Observable<[Post]> { get }
+}
+
+protocol PostViewModelType {
+    var output: PostViewModelOutputs { get }
+}
+
+class PostViewModel: PostViewModelOutputs {
+    
+    let posts: Observable<[Post]>
+    
+    private let disposeBag = DisposeBag()
+    private let provider = MoyaProvider<SnsAPI>()
+    
+    init() {
+        let _posts = PublishRelay<[Post]>()
+        self.posts = _posts.asObservable()
+        
+        PostRepository.getAllPosts()
+            .subscribe(onNext: { response in
+                _posts.accept(response)
+            }).disposed(by: disposeBag)
+    }
+}
+
+extension PostViewModel: PostViewModelType {
+    var output: PostViewModelOutputs { return self}
+}
