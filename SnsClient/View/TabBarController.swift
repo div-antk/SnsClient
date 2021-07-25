0669877d9d667d8554e9eef698e405c9bebf7d54
div-antk <51283468+owaii@users.noreply.github.com>
2021-07-25 23:12:21 +0900

    storyBoardでTabBarを作成

---
+++ SnsClient/View/TabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarController.swift
+//  SnsClient
+//
+//  Created by Takuya Ando on 2021/07/25.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let userVC = UIViewController()
+        let postVC = UIViewController()
+    }
+}
