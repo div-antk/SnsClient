8b4455257dc8173c39189d2779fd5565b0368053
div-antk <51283468+owaii@users.noreply.github.com>
2021-09-03 22:48:13 +0900

    Repositoryを作成しUserを取得するメソッドを記述

---
+++ SnsClient/Repository/UserRepository.swift
@@ -0,0 +1,27 @@
+//
+//  UserRepository.swift
+//  SnsClient
+//
+//  Created by Takuya Ando on 2021/09/03.
+//
+
+import Foundation
+import Moya
+import RxSwift
+
+final class UserRepository {
+    private static let apiProvider = MoyaProvider<SnsAPI>()
+    private static let disposeBag = DisposeBag()
+}
+
+extension UserRepository {
+    
+    static func getUser(id: String) -> Observable<[User]> {
+        return apiProvider.rx.request(.user(id: id))
+            .map { response in
+                print(response)
+                let decoder = JSONDecoder()
+                return try decoder.decode([User].self, from: response.data)
+            }.asObservable()
+    }
+}
