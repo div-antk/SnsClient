8fa7342e8f9676bc78d7139edc391270b65837cb
div-antk <51283468+owaii@users.noreply.github.com>
2021-09-09 19:21:03 +0900

    整形用のEntityの作成

---
+++ SnsClient/Entity/UserModel.swift
@@ -0,0 +1,15 @@
+//
+//  UserModel.swift
+//  SnsClient
+//
+//  Created by Takuya Ando on 2021/09/09.
+//
+
+import Foundation
+
+public struct UserModel: Codable {
+    let text: String
+    let _created_at: String
+    let _user_id: String
+    let User: User
+}
