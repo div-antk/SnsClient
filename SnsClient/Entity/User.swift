a5057b9cba607d4b682d5b3f777ab9deed5d8f53
div-antk <51283468+owaii@users.noreply.github.com>
2021-09-11 21:00:01 +0900

    Entityの修正

--- SnsClient/Entity/User.swift
+++ SnsClient/Entity/User.swift
@@ -5,10 +5,10 @@
 //  Created by Takuya Ando on 2021/09/03.
 //
 
-import Foundation
-
-public struct User: Codable {
-    let id: String
-    let description: String
-    let name: String
-}
+//import Foundation
+//
+//public struct User: Codable {
+//    let id: String
+//    let description: String
+//    let name: String
+//}
